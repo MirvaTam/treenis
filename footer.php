5ceb287acb439f27cbc1dba511c5c4c678ce5f5f
MirvaTam <mirva2112@gmail.com>
2021-10-13 13:46:41 +0300

    Eka commit.

---
+++ footer.php
@@ -0,0 +1,18 @@
+</div> <!-- tämä on koko sivun wrapperin sulku eli div class="wrapper" -->
+
+<footer>
+    <p>Kuulemme mielellään palautetta, jos esim. sivuston toiminnassa on jotakin kehitettävää, laita meille vapaasti viestiä.</p>
+<section class="palautelomake">
+    <form action="index.php?status=ok" method="post">
+        <input class="palaute" type="text" name="palaute" placeholder="Kirjoita palaute tähän" autocomplete="off">
+        <input class="palaute" type="text" name="email" placeholder="Anna email osoite, johon voimme olla yhteydessä" autocomplete="off">
+        <button class="palaute"type="submit" name="submit">Lähetä palaute</button>
+    </form>
+
+</section>
+</footer>
+<a id="logo" href="https://www.saromusiikki.fi/treenis" target="_blank"><img src="../Sopimussivusto/images/saro_vaaka.jpg" ></a>
+
+</body>
+
+</html>
